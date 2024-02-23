c638fe380bb703bba225ddb24680d74f3ff85d50
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-02-23 17:51:48 -0300

    Update script.js

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,23 +1,28 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "*", "/", "-", "+", "="]
-let output = "";
+const OPERATORS = ["%", "*", "/", "-", "+", "="];
+let currentInput = "";
 
-const calculate = (btnValue) => {
-  if(btnValue === "=" && output !== ""){
-    output = eval(output.replace("%", "/100"));
-    }else if(btnValue === "AC"){
-      output = ""
-    }else if(btnValue === "DEL"){
-      output = output.toString().slice(0,-1);
-    }else{
-      if(output === "" && specialChars.includes(btnValue)) return;
-      output += btnValue;
-    }
-    display.value = output;
-  };
+const calculate = (buttonValue) => {
+  if (buttonValue === "=" && currentInput !== "") {
+    // Calcula o resultado da expressão - Calculates the result of the expression
+    currentInput = eval(currentInput.replace("%", "/100"));
+  } else if (buttonValue === "AC") {
+    // Limpa a entrada atual
+    currentInput = "";
+  } else if (buttonValue === "DEL") {
+    // Remove o último caractere da entrada atual - Removes the last character from the current entry
+    currentInput = currentInput.toString().slice(0, -1);
+  } else {
+    // Adiciona o valor do botão à entrada atual - Adds the value of the button to the current entry
+    if (currentInput === "" && OPERATORS.includes(buttonValue)) return;
+    currentInput += buttonValue;
+  }
 
+  // Atualiza o valor exibido no display - Updates the value shown on the display
+  display.value = currentInput;
+};
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value))
-})
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
