2ad5d3b4b1cda2cdbe02fbbf072fd63615a2fb74
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-03-05 09:03:37 -0300

    update github icons

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -3,15 +3,23 @@ const darkTheme = "styles/style-dark.css"
 const sunICon = "assets/sun.svg"
 const moonICon = "assets/moon.svg"
 const themeIcon = document.getElementById("theme-icon")
+const githublightIcon = "assets/githlight.svg"
+const githubdarkIcon = "assets/githubdark.svg"
+const githubICon = document.getElementById("github-icon")
 
 function changeTheme() {
     const theme = document.getElementById("theme");
     if (theme.getAttribute("href") === lightTheme) {
         theme.setAttribute("href", darkTheme);
         themeIcon.setAttribute("src", sunICon);
+        githubICon.setAttribute("src", githublightIcon);
+
     } else {
         theme.setAttribute("href", lightTheme);
-        themeIcon.setAttribute("src", moonICon)
+        themeIcon.setAttribute("src", moonICon);
+        githubICon.setAttribute("src", githubdarkIcon);
     }
 }
+
+
 
