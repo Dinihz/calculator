7b5b2e118467431764ca0bd7953a60c17166b4c6
Lucas Souza Diniz <lucas87diniz@gmail.com>
2024-03-05 13:04:12 -0300

    javascript update
    new code

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -6,8 +6,8 @@ const themeIcon = document.getElementById("theme-icon")
 const githublightIcon = "assets/githlight.svg"
 const githubdarkIcon = "assets/githubdark.svg"
 const githubICon = document.getElementById("github-icon")
-const previusOperation = document.querySelector("#previous")
-const currentOperation = document.querySelector("current")
+const previusoperationText = document.querySelector("#previous")
+const currentoperationText = document.querySelector("#current")
 const buttons = document.querySelectorAll("#buttons-container button")
 
 function changeTheme() {
@@ -24,7 +24,153 @@ function changeTheme() {
     }
 }
 
+class Calculator {
+    constructor(previusoperationText, currentoperationText) {
+        this.previusoperationText = previusoperationText
+        this.currentoperationText = currentoperationText
+        this.currentOperation = ""
+    }
+    //add digit Oo screen
+    addDigit(digit) {
+        //check if current operation alredy has a dot
+        if(digit === "." && this.currentoperationText.innerText.includes(".")) {
+            return;
+        }
 
 
+        this.currentOperation = digit
+        this.updateScreen()
+    }
 
+    //Process all calculator operations
+    processOperation(operation) {
+        //check if current is empty
+        if(this.currentoperationText.innerText === "" && operation !== "C") {
+            //Change operation
+            if(this.previusoperationText.innerText !== "") {
+                this.changeOperation(operation);
+            }
+            return;
+        }
+
+        
+
+        //get current and previous value
+        let operationValue
+        const previous = +this.previusoperationText.innerText.split(" ")[0];
+        const current = +this.currentoperationText.innerText;
+
+        switch(operation) {
+        case "+":
+            operationValue = previous + current
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        case "-":
+            operationValue = previous - current
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        case "/":
+            operationValue = previous / current
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        case "*":
+            operationValue = previous * current
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        case "%":
+            operationValue = previous % current
+            this.updateScreen(operationValue, operation, current, previous);
+            break;
+        case "DEL":
+            this.processdelOperator();
+            break;
+        case "CE":
+            this.processclearcurrentOperaion();
+            break;
+        case "C":
+            this.processclearOperation();
+            break;
+        case "=":
+            this.processequalOperator();
+            break;
+        default:
+            return;
+        }
+    }
+
+    //chance values the screen
+    updateScreen(
+        operationValue = null, 
+        operation = null, 
+        current = null, 
+        previous = null
+        ) {
+        
+            console.log(operationValue, operation, current, previous)
+        
+            if(operationValue === null) {
+                this.currentoperationText.innerText += this.currentOperation;
+            } else {
+                //check if value is zero, if it is just add currrent value
+                if(previous === 0) {
+                    operationValue = current
+                }
+                
+                //add current value to previus
+                this.previusoperationText.innerText = `${operationValue} ${operation}`
+                this.currentoperationText.innerText = "";
+
+            }
+        }
+
+   //change math operation  
+    changeOperation(operation) {
+
+        const mathOperations = ["%", "*", "/", "+", "-"]
+
+        if(!mathOperations.includes(operation)) {
+            return;
+        }
+
+        this.previusoperationText.innerText = this.previusoperationText.innerText.slice(0, -1) + operation;
+
+    }
+
+    ///delete the last digit
+    processdelOperator() {
+        this.currentoperationText.innerText = this.currentoperationText.innerText.slice(0, -1)
+    }
+
+    //clar current operation
+    processclearcurrentOperaion() {
+        this.currentoperationText.innerText = "";
+    }
+
+    //clear all operations
+    processclearOperation() {
+        this.currentoperationText.innerText = "";
+        this.previusoperationText.innerText = "";
+    }
+
+    //process an operation
+    processequalOperator() {
+        const operation = previusoperationText.innerHTML.split(" ")[1];
+        this.processOperation(operation);
+    }
+
+}
+
+const calc = new Calculator(previusoperationText, currentoperationText)
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        if(+value >= 0 || value === ".") {
+            calc.addDigit(value)
+        } else {
+            calc.processOperation(value)
+        }
+    });
+});
 
